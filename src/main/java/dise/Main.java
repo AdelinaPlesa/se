38454bfca1d3b66478c1ad69733b25d4a2b05a22
adelinaplesa <you@emaple.com>
2021-09-10 14:41:23 +0300

    latest

--- src/main/java/dise/Main.java
+++ src/main/java/dise/Main.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 
-public class Maiin112 {
+public class Main {
 
     public static void main(String[] args) throws IOException {
 
